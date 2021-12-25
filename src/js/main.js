bfb01707090d1ff7abd10d5564f7a4d642156729
tictac <fominsartems947@gmail.com>
2021-12-25 23:45:53 +0200

    added adaptive to slider and made lightbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,45 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination,Autoplay  } from 'swiper';
+import { mobileIs } from './isMobile.js';
+import lightGallery from 'lightgallery';
 
 const burger = document.querySelector(".header__burger")
 const headerMenu = document.querySelector(".header__menu")
+
 burger.addEventListener("click", ()=> {
     burger.classList.toggle("header__burger--active")
     headerMenu.classList.toggle("header__menu--active")
 })
 
 new Swiper('.productivity', {
-    modules: [Pagination],
+    modules: [Autoplay, Pagination],
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
+        type: 'bullets',
+        clickable:true
+    },
+    autoplay: {
+        delay:2500,
+        disableOnInteraction:false,
     },
     loop:true,
-    autoplay: {
-        delay: 2000,
-    },
 });
 new Swiper('.blog__swiper', {
-    modules: [Navigation],
-    loop:true,
+    modules: [Navigation,Autoplay],
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    
+    loop:true,
     autoplay: {
-        delay:3000
+        delay:2500,
+        disableOnInteraction:false
     }
 })
-const isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
-if( isMobile.any() ) {
-    document.querySelector("body").classList.add("body__mobile")
-}
+if (mobileIs) document.querySelector("body").classList.add("body__mobile");
+document.querySelectorAll('.lightgallery').forEach(item => {
+    lightGallery(item,{
+        getCaptionFromTitleOrAlt: false
+    });
+    
+})
