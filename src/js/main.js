7b0c509398b5b9bedb18f209db6419f58b1f3453
tictac <fominsartems947@gmail.com>
2021-12-27 22:41:19 +0200

    finished dev part

--- src/js/main.js
+++ src/js/main.js
@@ -1,89 +1,92 @@
+
 import Swiper, { Navigation, Pagination,Autoplay  } from 'swiper';
 import { mobileIs } from './isMobile.js';
 import lightGallery from 'lightgallery';
 
-const burger = document.querySelector(".header__burger")
-const headerMenu = document.querySelector(".header__menu")
+window.addEventListener("load",()=>{
 
-burger.addEventListener("click", ()=> {
-    burger.classList.toggle("header__burger--active")
-    headerMenu.classList.toggle("header__menu--active")
-})
+    const burger = document.querySelector(".header__burger")
+    const headerMenu = document.querySelector(".header__menu")
 
-new Swiper('.productivity', {
-    modules: [Autoplay, Pagination],
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable:true
-    },
-    autoplay: {
-        delay:2500,
-        disableOnInteraction:false,
-    },
-    loop:true,
-});
-new Swiper('.blog__swiper', {
-    modules: [Navigation,Autoplay],
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    
-    loop:true,
-    autoplay: {
-        delay:2500,
-        disableOnInteraction:false
-    }
-})
-if (mobileIs) document.querySelector("body").classList.add("body__mobile");
-document.querySelectorAll('.lightgallery').forEach(item => {
-    lightGallery(item,{
-        getCaptionFromTitleOrAlt: false
-    });
-    
-})
+    burger.addEventListener("click", ()=> {
+        burger.classList.toggle("header__burger--active")
+        headerMenu.classList.toggle("header__menu--active")
+    })
 
-new Swiper('.slider-review__swiper', {
-    modules: [Pagination],
-    slidesPerView:1,
-    loop:true,
-    pagination: {
-        el: '.slider-review__pagination',
-        type: 'bullets',
-        clickable:true,
-    },
-    breakpoints: {
-        576: {
-            slidesPerView: "auto",
-            spaceBetween: 32,
+    new Swiper('.productivity', {
+        modules: [Autoplay, Pagination],
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable:true
         },
-    }
-})
-
-
-
-const accordionItems = document.querySelectorAll(".accordion__item-content");
-const heights = [...accordionItems].map(item=>item.clientHeight);
-[...accordionItems].forEach(item=>item.style.maxHeight = "0px");
-
-const triggers = document.querySelectorAll(".accordion__item-trigger")
-triggers.forEach((item,index)=>{
-    item.addEventListener("click",e=>{
-        const accordionParent = e.target.closest(".accordion");
-        if(accordionItems[index].classList.contains("accordion__item-content-active")) {
-            accordionItems[index].style.maxHeight =`${0}px`;
-            accordionItems[index].classList.remove("accordion__item-content-active")
-            item.classList.remove("accordion__item-trigger-active")
-        } else {
-            accordionParent.querySelectorAll(".accordion__item-content-active").forEach(item=>{
-                item.classList.remove("accordion__item-content-active")
-                item.style.maxHeight =`${0}px`;
-            })
-            accordionParent.querySelectorAll(".accordion__item-trigger").forEach(item=>item.classList.remove("accordion__item-trigger-active"))
-            accordionItems[index].style.maxHeight =`${heights[index]}px`;
-            accordionItems[index].classList.add("accordion__item-content-active")
-            item.classList.add("accordion__item-trigger-active")
+        autoplay: {
+            delay:2500,
+            disableOnInteraction:false,
+        },
+        loop:true,
+    });
+    new Swiper('.blog__swiper', {
+        modules: [Navigation,Autoplay],
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        
+        loop:true,
+        autoplay: {
+            delay:2500,
+            disableOnInteraction:false
         }
+    })
+    if (mobileIs) document.querySelector("body").classList.add("body__mobile");
+    document.querySelectorAll('.lightgallery').forEach(item => {
+        lightGallery(item,{
+            getCaptionFromTitleOrAlt: false
+        });
+        
+    })
+
+    new Swiper('.slider-review__swiper', {
+        modules: [Pagination],
+        slidesPerView:1,
+        loop:true,
+        pagination: {
+            el: '.slider-review__pagination',
+            type: 'bullets',
+            clickable:true,
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: "auto",
+                spaceBetween: 32,
+            },
+        }
+    })
+    
+    const accordionItems = document.querySelectorAll(".accordion__item-content");
+    const heights = [...accordionItems].map(item=>item.offsetHeight);
+    [...accordionItems].forEach(item=>item.style.maxHeight = "0px");
+    console.log(heights)
+
+    const triggers = document.querySelectorAll(".accordion__item-trigger")
+    triggers.forEach((item,index)=>{
+        item.addEventListener("click",e=>{
+            const accordionParent = e.target.closest(".accordion");
+            if(accordionItems[index].classList.contains("accordion__item-content-active")) {
+                accordionItems[index].style.maxHeight =`${0}px`;
+                accordionItems[index].classList.remove("accordion__item-content-active")
+                item.classList.remove("accordion__item-trigger-active")
+            } else {
+                accordionParent.querySelectorAll(".accordion__item-content-active").forEach(item=>{
+                    item.classList.remove("accordion__item-content-active")
+                    item.style.maxHeight =`${0}px`;
+                })
+                accordionParent.querySelectorAll(".accordion__item-trigger").forEach(item=>item.classList.remove("accordion__item-trigger-active"))
+                accordionItems[index].style.maxHeight =`${heights[index]}px`;
+                accordionItems[index].classList.add("accordion__item-content-active")
+                item.classList.add("accordion__item-trigger-active")
+            }
+        })
     })
 })
